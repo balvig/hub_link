06567b09a838d606ee5860c1c6bff53dd0ec0734
Jens Balvig <jens@balvig.com>
2019-03-18 11:55:34 +0900

    flesh out specs

--- test/api/pull_request_test.rb
+++ test/api/pull_request_test.rb
@@ -9,14 +9,16 @@ module HubLink::Api
     def test_review_requests
       pr = PullRequest.search("type:pr 64 repo:cookpad/cp8").first
 
-      assert_equal "balvig", pr.review_requests.first.requester
+      review_request = pr.review_requests.first
+
+      assert_equal "balvig", review_request.requester
+      assert_equal "tapster", review_request.reviewer
     end
 
     def test_reviews
       pr = PullRequest.search("type:pr 12 repo:balvig/hub_link").first
 
-      review = pr.reviews.first
-      require 'pry'; binding.pry
+      assert_equal "balvig", pr.reviews.first.user.login
     end
   end
 end
