846b96314806e2fb95023dd90cfa30eb10bdf45b
Jens Balvig <jens@balvig.com>
2019-05-15 15:05:23 +0900

    basic tests

--- test/stream_test.rb
+++ test/stream_test.rb
@@ -4,9 +4,9 @@ module HubLink
   class StreamTest < Minitest::Test
     def test_finding_in_batches
       Stream.new("cookpad/streamy", start_date: 1.week.ago).in_batches do |batch|
-        # puts batch.pull_requests
-        # puts batch.reviews
-        # puts batch.review_requests
+        puts batch.pull_requests
+        puts batch.reviews
+        puts batch.review_requests
       end
     end
   end
